5a4e7448635c384861f3cb26bc14be537bc6b6e9
Remi <remi.moucadeau@etu.univ-amu.fr>
2023-11-09 17:48:00 +0100

    Marketing page whit oraganisationnal folders

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas, Documents and Plans. Unified. Welcome to <span className="underline">Jotion</span>
+      </h1>
+      <h3>
+        Jotion is the connected workspace where <br />
+        better, faster work happens.
+      </h3>
+    </div>
+  );
+};
